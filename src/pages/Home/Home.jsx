f9057ca4a93f8112e2a5d5747b35528d56879edb
OSSwebmaster2003 <sodikovotabeksobirjonovich1509@gmail.com>
2023-02-21 00:49:15 -0800

    what we do section ready

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,6 +3,11 @@ import { Link } from 'react-router-dom';
 import { Banner, Swiper } from '../../components';
 import about_us from "../../assets/home/about_us.png";
 import video_player_icon from "../../assets/icons/video_play_icon.png";
+import what_we_do from "../../assets/home/what_we_do.png";
+import building from "../../assets/icons/building.png";
+import tree_planting from "../../assets/icons/tree_planting.png";
+import protect_land from "../../assets/icons/protect_land.png";
+import animal_safety from "../../assets/icons/animal_safety.png";
 import "./home.scss";
 
 function Home(props) {
@@ -41,6 +46,53 @@ function Home(props) {
           </div>
           <div className="supporters_info">
             <Swiper/>
+          </div>
+        </div>
+      </div>
+      <div className="what_we_do_section">
+        <div className="what_we_do_wrapper">
+          <div className="what_we_do_info">
+            <div className="empty_line"></div>
+            <div className="what_we_do_content">
+              <h3>What we do</h3>
+              <div className="what_we_do_header">
+                <h1>We care for earth, care for the coming birth</h1>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique.</p>
+              </div>
+              <div className="what_we_do_body">
+                <div className="what_we_do_card">
+                  <img src={building} alt="" />
+                  <div className="card_body">
+                    <h1>Build Healthy Cities</h1>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros.</p>
+                  </div>
+                </div>
+                <div className="what_we_do_card">
+                  <img src={tree_planting} alt="" />
+                  <div className="card_body">
+                    <h1>Tree Plantation</h1>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros.</p>
+                  </div>
+                </div>
+                <div className="what_we_do_card">
+                  <img src={protect_land} alt="" />
+                  <div className="card_body">
+                    <h1>Protect Land and Water</h1>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros.</p>
+                  </div>
+                </div>
+                <div className="what_we_do_card">
+                  <img src={animal_safety} alt="" />
+                  <div className="card_body">
+                    <h1>Animal Safety</h1>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros.</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="what_we_do_img">
+            <img src={what_we_do} alt="" />
           </div>
         </div>
       </div>
