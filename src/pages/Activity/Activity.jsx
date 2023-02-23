4f6b11bd3e201e0f3def4666b291f5f374a3c87a
OSSwebmaster2003 <sodikovotabeksobirjonovich1509@gmail.com>
2023-02-23 08:27:43 -0800

    what we do section finished

--- src/pages/Activity/Activity.jsx
+++ src/pages/Activity/Activity.jsx
@@ -1,10 +1,25 @@
 import React from 'react';
+import what_we_do from "../../assets/what_we_do/what_we_do.png";
 import "./activity.scss";
 
 function Activity(props) {
   return (
-    <div>
-      Activity
+    <div className='activity_page'>
+      <div className="what_we_do_section">
+        <div className="what_we_do_info">
+          <div className="info_header">
+            <div className="empty_line"></div>
+            <h3>What we do</h3>
+          </div>
+          <div className="info_body">
+            <h1>We are working worldwide</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique. Duis cursus, mi quis viverra ornare, eros dolor interdum nulla, ut commodo diam libero vitae erat.</p>
+          </div>
+        </div>
+        <div className="what_we_do_img">
+          <img src={what_we_do} alt="" />
+        </div>
+      </div>
     </div>
   );
 }
