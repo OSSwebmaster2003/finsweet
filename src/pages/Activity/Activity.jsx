41b4ec713c1bc00f4694242ccf3b78c1b3d94632
OSSwebmaster2003 <sodikovotabeksobirjonovich1509@gmail.com>
2023-02-23 09:04:52 -0800

    for the environment section finished

--- src/pages/Activity/Activity.jsx
+++ src/pages/Activity/Activity.jsx
@@ -1,5 +1,11 @@
 import React from 'react';
 import what_we_do from "../../assets/what_we_do/what_we_do.png";
+import building from "../../assets/icons/building.png";
+import protect_land from "../../assets/icons/protect_land.png";
+import tree_planting from "../../assets/icons/tree_planting.png";
+import water from "../../assets/icons/water.png";
+import animal_safety from "../../assets/icons/animal_safety.png";
+import biodiversity from "../../assets/icons/biodiversity.png";
 import "./activity.scss";
 
 function Activity(props) {
@@ -18,6 +24,57 @@ function Activity(props) {
         </div>
         <div className="what_we_do_img">
           <img src={what_we_do} alt="" />
+        </div>
+      </div>
+      <div className="for_environment_section">
+        <div className="environment_wrapper">
+          <div className="environment_header">
+            <h1>What we do for the environment</h1>
+          </div>
+          <div className="environment_info">
+            <div className="environment_card">
+              <img src={building} alt="" />
+              <div className="card_info">
+                <h2>Build healthy cities</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique.</p>
+              </div>
+            </div>
+            <div className="environment_card">
+              <img src={protect_land} alt="" />
+              <div className="card_info">
+                <h2>Protect land and water</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique.</p>
+              </div>
+            </div>
+            <div className="environment_card">
+              <img src={tree_planting} alt="" />
+              <div className="card_info">
+                <h2>Tree plantation</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique.</p>
+              </div>
+            </div>
+            <div className="environment_card">
+              <img src={water} alt="" />
+              <div className="card_info">
+                <h2>Water sustainability</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique.</p>
+              </div>
+            </div>
+            <div className="environment_card">
+              <img src={animal_safety} alt="" />
+              <div className="card_info">
+                <h2>Animal safety</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique.</p>
+              </div>
+            </div>
+            <div className="environment_card">
+              <img src={biodiversity} alt="" />
+              <div className="card_info">
+                <h2>Biodiversity</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique.</p>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
