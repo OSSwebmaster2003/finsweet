84b3e3fdb62dcf215dc0805c13ac6af3547d8ada
OSSwebmaster2003 <sodikovotabeksobirjonovich1509@gmail.com>
2023-03-09 01:03:44 -0800

    some links added

--- src/components/JoinTeam/JoinTeam.jsx
+++ src/components/JoinTeam/JoinTeam.jsx
@@ -1,19 +1,19 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 import join_team from "../../assets/home/join_team.png";
 import "./joinTeam.scss";
 
 function JoinTeam(props) {
   return (
-    <div className='join_team' style={{ backgroundImage: `url(${join_team})` }}>
+    <div className="join_team" style={{ backgroundImage: `url(${join_team})` }}>
       <div className="join_team_content">
         <h1>You can contribute to make the environment greener!</h1>
         <div className="join_team_content_buttons">
           <button className="volunteer_btn">
-            <Link>Join as a volunteer</Link>
+            <Link to="/contact">Join as a volunteer</Link>
           </button>
           <button className="donation_btn">
-            <Link>Donate</Link>
+            <Link to="/donation">Donate</Link>
           </button>
         </div>
       </div>
