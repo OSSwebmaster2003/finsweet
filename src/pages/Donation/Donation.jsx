067ced90c2b9d85302acdd317b7d3a1e8cdf37aa
OSSwebmaster2003 <sodikovotabeksobirjonovich1509@gmail.com>
2023-03-08 23:56:11 -0800

    donation usage section finished

--- src/pages/Donation/Donation.jsx
+++ src/pages/Donation/Donation.jsx
@@ -47,6 +47,25 @@ function Donation(props) {
             </div>
           </div>
         </div>
+        <div className="donation_usage_section">
+          <div className="donation_usage_wrapper">
+            <div className="usage_header">How we use your donation</div>
+            <div className="usage_body">
+              <div className="first_part">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                Suspendisse varius enim in eros elementum tristique. Duis
+                cursus, mi quis viverra ornare, eros dolor interdum nulla, ut
+                commodo diam libero vitae erat. Nunc ut sem vitae risus
+                tristique posuere.
+              </div>
+              <div className="second_part">
+                Aenean faucibus nibh et justo cursus id rutrum lorem imperdiet.
+                Nunc ut sem vitae risus tristique posuere. Aenean faucibus nibh
+                et justo cursus id rutrum lorem imperdiet.
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
