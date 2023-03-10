6174228626b1fa35474f38e920d610cc7162f1ed
OSSwebmaster2003 <sodikovotabeksobirjonovich1509@gmail.com>
2023-03-10 02:48:30 -0800

    mobile toggle error fixed

--- src/common/Header/Header.jsx
+++ src/common/Header/Header.jsx
@@ -1,37 +1,39 @@
-import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
-import {AiOutlineAlignLeft} from "react-icons/ai";
-import {BsXLg} from "react-icons/bs";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import { AiOutlineAlignLeft } from "react-icons/ai";
+import { BsXLg } from "react-icons/bs";
 import logo from "../../assets/main/logo.png";
 import "./header.scss";
 
 function Header(props) {
-  const [mobileToggle , setMobileToggle] = useState(false);
+  const [mobileToggle, setMobileToggle] = useState(false);
   const handleMobileToggle = () => {
-    setMobileToggle(prev => !prev)
-  }
+    setMobileToggle((prev) => !prev);
+  };
   return (
     <header className="header">
       <div className="header_wrapper">
         <div className="logo">
-          <Link to="/"><img src={logo} alt="" /></Link>
+          <Link to="/">
+            <img src={logo} alt="" />
+          </Link>
         </div>
         <nav className="navbar">
-          <ul style={!mobileToggle ? {right : "-500px"} : {right : "0"} }>
-            <BsXLg onClick={() => handleMobileToggle()}/>
-            <li>
+          <ul style={!mobileToggle ? { right: "-500px" } : { right: "0" }}>
+            <BsXLg onClick={() => handleMobileToggle()} />
+            <li onClick={() => handleMobileToggle()}>
               <Link to="/">Home</Link>
             </li>
-            <li>
+            <li onClick={() => handleMobileToggle()}>
               <Link to="/about">About us</Link>
             </li>
-            <li>
+            <li onClick={() => handleMobileToggle()}>
               <Link to="/activity">What We Do</Link>
             </li>
-            <li>
+            <li onClick={() => handleMobileToggle()}>
               <Link to="/media">Media</Link>
             </li>
-            <li>
+            <li onClick={() => handleMobileToggle()}>
               <Link to="/contact">Contact</Link>
             </li>
           </ul>
@@ -40,7 +42,7 @@ function Header(props) {
           </button>
         </nav>
         <div className="toggle_mobile">
-          <AiOutlineAlignLeft onClick={() => handleMobileToggle()}/>
+          <AiOutlineAlignLeft onClick={() => handleMobileToggle()} />
         </div>
       </div>
     </header>
